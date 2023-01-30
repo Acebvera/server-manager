e04113525ce84149ebdffba4e56f5bb6349d8c44
alex <alex@gmail.com>
2023-01-30 18:09:55 +0100

    Añadida funcion ping() en interfaz ServerService.java. Creada implementación ServerServiceImplementation.java

--- src/main/java/es/Acebera/server/service/ServerService.java
+++ src/main/java/es/Acebera/server/service/ServerService.java
@@ -1,11 +1,13 @@
 package es.Acebera.server.service;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import es.Acebera.server.model.Server;
 
 public interface ServerService {
 	Server create(Server server);
+	Server ping (String ipAdress) throws IOException;
 	Collection<Server> list (int limit); //Limitar el numero de servidores listados
 	Server get (Long id);
 	Server update (Server server);
---
+++ src/main/java/es/Acebera/server/service/implementation/ServerServiceImplementation.java
@@ -0,0 +1,76 @@
+package es.Acebera.server.service.implementation;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.Random;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import es.Acebera.server.enumeration.Status;
+import es.Acebera.server.model.Server;
+import es.Acebera.server.repo.ServerRepo;
+import es.Acebera.server.service.ServerService;
+
+@Service
+@Transactional
+public class ServerServiceImplementation implements ServerService{
+	private ServerRepo serverRepo;
+	
+	public ServerServiceImplementation (ServerRepo serverRepo) {
+		this.serverRepo = serverRepo;
+	}
+	
+	@Override
+	public Server create(Server server) {
+		System.out.println("Saving new server: " + server.getName());
+		server.setImageUrl(setServerImageUrl());
+		return serverRepo.save(server);
+	}
+	
+	@Override
+	public Server ping(String ipAdress) throws IOException {
+		System.out.println("Pinging server IP: " + ipAdress);
+		Server server = serverRepo.findByIpAdress(ipAdress);
+		InetAddress address = InetAddress.getByName(ipAdress);
+		server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+		serverRepo.save(server);
+		return server;
+	}
+
+	@Override
+	public Collection<Server> list(int limit) {
+		System.out.println("Fetching all servers");
+
+		return serverRepo.findAll(PageRequest.of(0, limit)).toList();
+	}
+
+	@Override
+	public Server get(Long id) {
+		System.out.println("Fetching server by ID " + id);
+		return serverRepo.findById(id).get();
+	}
+
+	@Override
+	public Server update(Server server) {
+		System.out.println("Updating server: " + server.getName());
+		
+		return serverRepo.save(server);
+	}
+
+	@Override
+	public Boolean delete(Long id) {
+		System.out.println("Deleting server by ID: " + id);
+		serverRepo.deleteById(id);
+		return true;
+	}
+
+	private String setServerImageUrl() {
+		String [] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+		return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
+	}
+	
+}
