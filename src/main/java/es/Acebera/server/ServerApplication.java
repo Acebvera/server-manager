fe0b94a3efae0d92dac1f2c428eb7692ea52926e
alex <alex@gmail.com>
2023-02-03 20:34:45 +0100

    Creados datos de ejemplo que se añadirán al iniciar el programa en ServerApplication.java

--- src/main/java/es/Acebera/server/ServerApplication.java
+++ src/main/java/es/Acebera/server/ServerApplication.java
@@ -1,13 +1,30 @@
 package es.Acebera.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import es.Acebera.server.enumeration.Status;
+import es.Acebera.server.model.Server;
+import es.Acebera.server.repo.ServerRepo;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner run (ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "8 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.51", "Fedora Linux", "32 GB", "Dell Tower", "http://localhost:8080/server/image/server2.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.28", "Microsoft Windows Server 2012", "128 GB", "Web Server", "http://localhost:8080/server/image/server3.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", Status.SERVER_UP));
+			
+		};
 	}
 
 }
